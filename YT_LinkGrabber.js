56a53517c1ea3fd8d039d417a3bc6b97577c82dc
syllent <42889432+syllent@users.noreply.github.com>
2020-05-09 22:46:10 +0300

    Update YT_LinkGrabber.js

--- YT_LinkGrabber.js
+++ YT_LinkGrabber.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         YT LinkGrabber
 // @namespace    YT
-// @version      1.0.4
+// @version      1.0.3.1
 // @description  fk u
 // @author       BogLev
 // @require      http://code.jquery.com/jquery-3.5.1.min.js
